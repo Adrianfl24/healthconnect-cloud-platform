f3e98b7e69a83748e1f7779787a9846b54d20ce7
Adrianfl24 <adrianfloricescu1@gmail.com>
2026-07-09 17:44:48 +0300

    Initial commit: Adaugare schelet Java Spring Boot

---
+++ src/test/java/com/healthconnect/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthconnect.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
